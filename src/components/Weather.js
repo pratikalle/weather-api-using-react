87df0bba8dd978d35d02e86cd846ffb0b901128c
pratik.alle <pratik.alle@bauer.de>
2023-10-17 10:07:11 +0530

    initial commit

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,73 +1,65 @@
-import React, { useState } from "react";
-import DisplayWeather from "./DisplayWeather";
+import React, {useState} from "react";
 import "./weather.css";
+import DisplayWeather from "./DisplayWeather";
+
+//https://openweathermap.org/current#name -- url for weather api
 
 function Weather() {
-  const [weather, setWeather] = useState([]);
+
+  const apikey = "4e7cc393e29ffc9e6ecf2a4df832434d";
+
   const [form, setForm] = useState({
     city: "",
-    country: "",
-  });
+    country: ""
+  })
 
-  const APIKEY = "Enter Your API Key";
-  
-  async function weatherData(e) {
+  const [weather, setWeather] = useState([])
+
+  async function weatherData(e){
     e.preventDefault();
-    if (form.city === "") {
-      alert("Add values");
-    } else {
-      const data = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${form.city},${form.country}&APPID=${APIKEY}`
-      )
-        .then((res) => res.json())
-        .then((data) => data);
 
-      setWeather({ data: data });
+    if(form.city == ''){
+      alert('First provide all the required fields');
+    }else{
+      const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${form.city},${form.country}&appid=${apikey}`).then(res => res.json()).then((data) => data);
+
+      setWeather(
+        {
+          data : data
+        }
+      );
     }
   }
 
   const handleChange = (e) => {
     let name = e.target.name;
     let value = e.target.value;
-
-    if (name === "city") {
-      setForm({ ...form, city: value });
+    if(name == 'city'){
+      setForm({...form, city:value})
+    }else{
+      setForm({...form, country:value})
     }
-    if (name === "country") {
-      setForm({ ...form, country: value });
-    }
-  };
-  return (
-    <div className="weather">
-      <span className="title">Weather App</span>
-      <br />
-      <form>
-        <input
-          type="text"
-          placeholder="city"
-          name="city"
-          onChange={(e) => handleChange(e)}
-        />
-        &nbsp; &nbsp; &nbsp;&nbsp;
-        <input
-          type="text"
-          placeholder="Country"
-          name="country"
-          onChange={(e) => handleChange(e)}
-        />
-        <button className="getweather" onClick={(e) => weatherData(e)}>
-          Submit
-        </button>
-      </form>
 
-      {/* {console.log(weather)} */}
-      {weather.data !== undefined ? (
-        <div>
-          <DisplayWeather data={weather.data} />
-        </div>
+  }
+
+  return <div className="weather">
+    <span className="title">
+      Weather App
+    </span>
+    <br/>
+
+    <form>
+      <input type="text" name="city" onChange={e => handleChange(e)} placeholder="Enter City"/>
+      &nbsp; &nbsp;
+      <input type="text" name="country" onChange={e => handleChange(e)} placeholder="Enter Country"/>
+      <button className="getweather" onClick={(e) => weatherData(e)}>Submit</button>
+    </form>
+
+    {weather.data != undefined ? (
+      <div><DisplayWeather data={weather.data} />
+      </div>
       ) : null}
-    </div>
-  );
+  </div>
 }
 
 export default Weather;
